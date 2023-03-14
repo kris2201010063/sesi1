625e65200222a563c01e51996b68801adb7c94d9
kris2201010063 <127836411+kris2201010063@users.noreply.github.com>
2023-03-14 16:56:04 +0800

    sesi1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML latihan 1</title>
+</head>
+<body>
+
+    <h3>Belajar web  Programing<?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>menggunakan Script javascript</h3>");
+    </script>
+
+    <?php
+         echo "<h3>Menggunakan Script PHP</h3>";
+    ?>
+</body>
+</html>
